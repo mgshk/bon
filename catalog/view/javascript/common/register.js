77a0126f92d3dea5edfd027bd956a117c9cade1a
Magesh K <mgshk.1507@gmail.com>
2017-10-19 16:31:37 +0530

    added register js

---
+++ catalog/view/javascript/common/register.js
@@ -0,0 +1,50 @@
+function getZones(val) {
+  $.ajax({
+      url: 'index.php?route=account/account/country&country_id=' + val,
+      dataType: 'json',
+      beforeSend: function() {
+        $('select[name=\'register_country_id\']').after(' <i class="fa fa-circle-o-notch fa-spin"></i>');
+      },
+      complete: function() {
+        $('.fa-spin').remove();
+      },
+      success: function(json) {
+        var html = '<option value="0"> --- Please Select --- </option>';
+
+        if (json['zone'] && json['zone'] != '') {
+          for (i = 0; i < json['zone'].length; i++) {
+            html += '<option value="' + json['zone'][i]['zone_id'] + '"';
+
+            if (json['zone'][i]['zone_id'] == $('#hidden_register_zoneId').val()) {
+              html += ' selected="selected"';
+            }
+
+            html += '>' + json['zone'][i]['name'] + '</option>';
+          }
+        }
+
+        $('select[name=\'register_zone_id\']').html(html);
+      },
+      error: function(xhr, ajaxOptions, thrownError) {
+        alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+      }
+  });
+
+  return false;
+}
+
+$(document).ready(function() {
+  $(document).on('change', 'select[name=\'register_country_id\']', function() {
+    getZones($(this).val());
+  });
+
+  $('select[name=\'register_country_id\']').trigger('change');
+
+  $(document).on('change', 'input[name=\'payment\']', function() {
+    $('.payment').hide();
+
+    $('#payment-' + this.value).show();
+  });
+
+  $('input[name=\'payment\']:checked').trigger('change');
+});
